847f4012833cd2099698dc0ce72c38bbca31d21a
Mark Newman <mwnewman@umich.edu>
2020-09-25 08:28:02 -0400

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,132 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+let num = 13;
+
+export function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Press Me!"
+        color="blue"
+        onPress={()=>console.log("I'm pressed!")}/>
     </View>
   );
+}
+
+export function App1() {
+  let num = 10;
+  return (
+    <View style={{
+        backgroundColor: 'fff', 
+        top: 200, 
+        left: 100, 
+        width: 200 }}>
+      <Text style>Your lucky number:</Text>
+      <Text style>{num}</Text>
+    </View>
+  );
+}
+
+export class App2 extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+export class App3 extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button 
+          title="+"
+          color="red"
+          onPress={()=>{
+            this.setState({counter: this.state.counter + 1}
+          )}}
+        />
+        <Text>{this.state.counter}</Text>
+      </View>
+    );
+  }
+}
+
+export default class App4 extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button 
+          title="+"
+          color="red"
+          size="40"
+          onPress={()=>{
+            this.setState({counter: this.state.counter + 1}
+          )}}
+        />
+        <Text>{this.state.counter}</Text>
+      </View>
+    );
+  }
+}
+
+
+export class App5 extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity 
+          style={{
+            height: 100,
+            width: 200,
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 10,
+            backgroundColor: 'lightblue',
+            borderRadius:10,
+            borderWidth: 3,
+            borderColor: '#111'
+          }}
+          onPress={()=>{
+            this.setState({counter: this.state.counter + 1}
+          )}}
+        >
+          <Text style={{fontSize: 36}}>{this.state.counter}</Text>
+          <Text style={{fontSize: 12}}>Press me!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
